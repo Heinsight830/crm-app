2de53581a1d9e7ba570a49be5549a9374e057c44
Heinsight830 <info@vmag4vets.com>
2026-04-09 06:07:28 -0500

    Update CreateRenewal.php

--- app/Filament/Resources/Renewals/Pages/Pages/CreateRenewal.php
+++ app/Filament/Resources/Renewals/Pages/Pages/CreateRenewal.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace App\Filament\Resources\Renewals\Pages;
+use App\Filament\Resources\Renewals\RenewalResource;
+use Filament\Resources\Pages\CreateRecord;
+class CreateRenewal extends CreateRecord
+{
+    protected static string $resource = RenewalResource::class;
+}
