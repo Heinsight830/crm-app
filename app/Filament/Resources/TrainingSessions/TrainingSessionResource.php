ce78a3fcce1c4e03132a6ae91675b820fc685c1b
Heinsight830 <info@vmag4vets.com>
2026-04-09 05:48:44 -0500

    Update TrainingSessionResource.php

--- app/Filament/Resources/TrainingSessions/TrainingSessionResource.php
+++ app/Filament/Resources/TrainingSessions/TrainingSessionResource.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Filament\Resources\TrainingSessions;
-
 use App\Filament\Resources\TrainingSessions\Pages\CreateTrainingSession;
 use App\Filament\Resources\TrainingSessions\Pages\EditTrainingSession;
 use App\Filament\Resources\TrainingSessions\Pages\ListTrainingSessions;
@@ -13,40 +11,31 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-
 class TrainingSessionResource extends Resource
 {
     protected static ?string $model = TrainingSession::class;
-
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
-
     protected static ?string $recordTitleAttribute = 'title';
-
     public static function shouldRegisterNavigation(): bool
     {
         return auth()->check() && auth()->user()->hasAnyRole(['admin', 'trainer']);
     }
-
     public static function canAccess(): bool
     {
         return auth()->check() && auth()->user()->hasAnyRole(['admin', 'trainer']);
     }
-
     public static function form(Schema $schema): Schema
     {
         return TrainingSessionForm::configure($schema);
     }
-
     public static function table(Table $table): Table
     {
         return TrainingSessionsTable::configure($table);
     }
-
     public static function getRelations(): array
     {
         return [];
     }
-
     public static function getPages(): array
     {
         return [
