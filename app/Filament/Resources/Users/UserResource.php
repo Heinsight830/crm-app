3ec1978b88941b2099cd1567dcac056096242d4d
Heinsight830 <info@vmag4vets.com>
2026-04-09 05:49:17 -0500

    Update UserResource.php

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Filament\Resources\Users;
-
 use App\Filament\Resources\Users\Pages\CreateUser;
 use App\Filament\Resources\Users\Pages\EditUser;
 use App\Filament\Resources\Users\Pages\ListUsers;
@@ -13,42 +11,31 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
-
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
     protected static ?string $recordTitleAttribute = 'name';
-
-    protected static ?string $navigationLabel = 'Users';
-
     public static function shouldRegisterNavigation(): bool
     {
-        return auth()->check() && auth()->user()->hasRole('admin');
+        return auth()->check() && auth()->user()->hasAnyRole(['admin']);
     }
-
     public static function canAccess(): bool
     {
-        return auth()->check() && auth()->user()->hasRole('admin');
+        return auth()->check() && auth()->user()->hasAnyRole(['admin']);
     }
-
     public static function form(Schema $schema): Schema
     {
         return UserForm::configure($schema);
     }
-
     public static function table(Table $table): Table
     {
         return UsersTable::configure($table);
     }
-
     public static function getRelations(): array
     {
         return [];
     }
-
     public static function getPages(): array
     {
         return [
