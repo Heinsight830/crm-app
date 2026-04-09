3703a966ea14d997768f6ec49ce87f4642e8312f
Heinsight830 <info@vmag4vets.com>
2026-04-09 05:48:24 -0500

    Update PaymentResource.php

--- app/Filament/Resources/Payments/PaymentResource.php
+++ app/Filament/Resources/Payments/PaymentResource.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Filament\Resources\Payments;
-
 use App\Filament\Resources\Payments\Pages\CreatePayment;
 use App\Filament\Resources\Payments\Pages\EditPayment;
 use App\Filament\Resources\Payments\Pages\ListPayments;
@@ -13,40 +11,31 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-
 class PaymentResource extends Resource
 {
     protected static ?string $model = Payment::class;
-
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
-
     protected static ?string $recordTitleAttribute = 'reference';
-
     public static function shouldRegisterNavigation(): bool
-{
-    return auth()->check() && auth()->user()->hasAnyRole(['admin']);
-}
-
+    {
+        return auth()->check() && auth()->user()->hasAnyRole(['admin']);
+    }
     public static function canAccess(): bool
-{
-    return auth()->check() && auth()->user()->hasAnyRole(['admin']);
-}
-
+    {
+        return auth()->check() && auth()->user()->hasAnyRole(['admin']);
+    }
     public static function form(Schema $schema): Schema
     {
         return PaymentForm::configure($schema);
     }
-
     public static function table(Table $table): Table
     {
         return PaymentsTable::configure($table);
     }
-
     public static function getRelations(): array
     {
         return [];
     }
-
     public static function getPages(): array
     {
         return [
