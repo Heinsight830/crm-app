26339acf3a9e085829716a8cff9d17ca9caed4dd
Heinsight830 <info@vmag4vets.com>
2026-04-09 06:06:40 -0500

    Update ListRenewals.php

--- app/Filament/Resources/Renewals/Pages/ListRenewals.php
+++ app/Filament/Resources/Renewals/Pages/ListRenewals.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace App\Filament\Resources\Renewals\Pages;
+use App\Filament\Resources\Renewals\RenewalResource;
+use Filament\Resources\Pages\ListRecords;
+class ListRenewals extends ListRecords
+{
+    protected static string $resource = RenewalResource::class;
+}
