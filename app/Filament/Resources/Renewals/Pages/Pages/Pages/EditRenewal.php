8380c753e5e328c42363138f8ff5c441ec14b59c
Heinsight830 <info@vmag4vets.com>
2026-04-09 06:10:43 -0500

    Update EditRenewal.php

--- app/Filament/Resources/Renewals/Pages/Pages/Pages/EditRenewal.php
+++ app/Filament/Resources/Renewals/Pages/Pages/Pages/EditRenewal.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace App\Filament\Resources\Renewals\Pages;
+use App\Filament\Resources\Renewals\RenewalResource;
+use Filament\Resources\Pages\EditRecord;
+class EditRenewal extends EditRecord
+{
+    protected static string $resource = RenewalResource::class;
+}
